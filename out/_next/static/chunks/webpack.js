f33a993467c2641a703ec5c5db56f94740303eeb
deadit <stdeadit@gmail.com>
2021-02-26 12:52:17 +0300

    update site

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0c732b199c33d7ea64d8";
+/******/ 	var hotCurrentHash = "450c16237826ebd51674";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
