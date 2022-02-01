c7e70d1f01fa5b91bcceac94977905e2ebff2827
deadit <stdeadit@gmail.com>
2022-02-01 17:44:29 +0300

    add admin panel

---
+++ pages/admin.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+  return (
+    <>
+      <script src="https://unpkg.com/netlify-cms@^2.10.49/dist/netlify-cms.js"></script>
+    </>
+  );
+};
+
+export default AdminPage;
