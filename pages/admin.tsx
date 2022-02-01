92f225d4a0e2a07175f1d597f3d2222d078479f9
deadit <stdeadit@gmail.com>
2022-02-01 18:39:49 +0300

    update admin panel

--- pages/admin.tsx
+++
@@ -1,9 +0,0 @@
-const AdminPage = () => {
-  return (
-    <>
-      <script src="https://unpkg.com/netlify-cms@^2.10.49/dist/netlify-cms.js"></script>
-    </>
-  );
-};
-
-export default AdminPage;
