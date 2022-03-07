2fdb23191e3716ba70f4c92f74339b78768a1fa3
Kosteva Vera <kosteva.vera@gmail.com>
2022-03-07 17:44:00 +0700

    Resolve conflict

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,8 +1,7 @@
-import React /*, { useRef }*/ from 'react';
+import React from 'react';
 import SearchIconSvg from './SearchIconSvg';
 
-export default function SearchBar({ searchedValue, setSearchedValue }: any) {/*  const ref = useRef<any>();*/
-
+export default function SearchBar({ searchedValue, setSearchedValue }: any) {
   return (
     <div className="search">
       <SearchIconSvg />
@@ -13,42 +12,6 @@ export default function SearchBar({ searchedValue, setSearchedValue }: any) {/* 
         type="text"
         placeholder="Search for DApps"
       />
-      {/*      <div className="filter__container">
-        <button
-          className="right_button"
-          onClick={() => {
-            ref.current.scrollLeft -= 120;
-          }}
-        />
-        <div className="filter" ref={ref}>
-          {[
-            'All',
-            'dApps',
-            'Privacy',
-            'NFTs',
-            'Metaverse',
-            'Explore',
-            'Mining',
-            'Tooling',
-            'Tokens',
-
-          ].map((e) => (
-            <div
-              key={e}
-              className={e == filter ? 'filter__active' : 'filter__tag'}
-              onClick={() => setFilter(e)}
-            >
-              {e}
-            </div>
-          ))}
-        </div>
-        <button
-          className="left_button"
-          onClick={() => {
-            ref.current.scrollLeft += 120;
-          }}
-        />
-      </div>*/}
     </div>
   );
 }
