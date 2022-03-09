268ee56e2313d04c1e6fad13fc0651abcbaf7421
deadit <stdeadit@gmail.com>
2022-03-09 21:21:21 +0300

    update categories

--- components/Filters/filters.tsx
+++ components/Filters/filters.tsx
@@ -5,43 +5,43 @@ export default function Filters({ filter, setFilter }: any) {
 
   return (
     <div className="filter">
-    <h1>Explore the universe of Sigmaverse</h1>
-    <div className="filter__container">
-      <button
-        className="left_button"
-        onClick={() => {
-          ref.current.scrollLeft -= 120;
-        }}
-      />
-      <div className="filter-item" ref={ref}>
-        {[
-          'All',
-          'dApps',
-          'Privacy',
-          'NFTs',
-          'Metaverse',
-          'Explore',
-          'Mining',
-          'Tooling',
-          'Tokens',
-
-        ].map((e) => (
-          <div
-            key={e}
-            className={e == filter ? 'filter-item__active' : 'filter-item__tag'}
-            onClick={() => setFilter(e)}
-          >
-            {e}
-          </div>
-        ))}
+      <h1>Explore the universe of Sigmaverse</h1>
+      <div className="filter__container">
+        <button
+          className="left_button"
+          onClick={() => {
+            ref.current.scrollLeft -= 120;
+          }}
+        />
+        <div className="filter-item" ref={ref}>
+          {[
+            'All',
+            'dApps',
+            'Privacy',
+            'NFTs',
+            'Metaverse',
+            'Explore',
+            'Mining',
+            'Tooling',
+            'Tokens',
+            'Wallets',
+          ].map((e) => (
+            <div
+              key={e}
+              className={e == filter ? 'filter-item__active' : 'filter-item__tag'}
+              onClick={() => setFilter(e)}
+            >
+              {e}
+            </div>
+          ))}
+        </div>
+        <button
+          className="right_button"
+          onClick={() => {
+            ref.current.scrollLeft += 120;
+          }}
+        />
       </div>
-      <button
-        className="right_button"
-        onClick={() => {
-          ref.current.scrollLeft += 120;
-        }}
-      />
-    </div>
     </div>
   );
 }
