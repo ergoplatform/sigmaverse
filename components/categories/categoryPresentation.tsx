5c19d6d9c07f9c8e3b35c02d3896c26065acf93f
deadit <stdeadit@gmail.com>
2022-03-09 20:27:27 +0300

    update cards

--- components/categories/categoryPresentation.tsx
+++ components/categories/categoryPresentation.tsx
@@ -4,7 +4,7 @@ export default function CategoryPresentation({ data }: any) {
   return (
     <>
       {data.map(({ logo, preview, name, description, website }: any) => (
-        <a key={website} href={website} target="_blank" className="dapps-card">
+        <div key={website} className="dapps-card">
           <div className="dapps-card__preview">
             <img src={preview} alt={`${name} preview`} />
           </div>
@@ -12,8 +12,12 @@ export default function CategoryPresentation({ data }: any) {
             <img src={logo} alt={`${name} logotype`} className="dapps-card__logotype" />
             <div className="dapps-card__name">{name}</div>
             <p className="dapps-card__description">{description}</p>
+            <div className="dapps-card__divider"></div>
+            <a href={website} target="_blank" className="dapps-card__link">
+              Go to project
+            </a>
           </div>
-        </a>
+        </div>
       ))}
     </>
   );
