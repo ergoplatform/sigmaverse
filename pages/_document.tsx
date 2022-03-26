649040af75113a995f00eca028bc5449ddf36553
deadit <stdeadit@gmail.com>
2022-03-26 14:52:44 +0300

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,23 +22,18 @@ class MyDocument extends Document {
           <meta name="language" content="English" />
           <meta name="author" content="ergoplatform.org" />
           <link rel="shortcut icon" href="/static/favicon.svg" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-VT7PYD147T"></script>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-MFCKL3B');`,
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-VT7PYD147T');`,
             }}
           ></script>
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFCKL3B"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
           <Main />
           <NextScript />
         </body>
