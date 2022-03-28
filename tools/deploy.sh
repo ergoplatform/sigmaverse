5b30cf96f93135ed4f3783bcbacfc6749504cb03
deadit <stdeadit@gmail.com>
2022-03-28 18:29:26 +0300

    update deploy config

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start deploy"
-ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/sigmaverse/;git fetch; git checkout master; git pull --force; yarn; yarn build; npx next export"'
+ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/sigmaverse/;git fetch; git checkout master; git pull --force; yarn; yarn build & npx next export"'
 echo "Deployed Successfully!"
 
 exit 0
